9d58f5b3ae29ee3dd21bbe798dc30e3f836d3f11
silljaco <161739321+silljaco@users.noreply.github.com>
2024-02-29 20:36:56 +0100

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # meters
+height_reduction = 3/5
+i = 1
+
+while i < 11:
+    height = height * height_reduction
+    i = i+1
+    print(i, round(height, 2))
