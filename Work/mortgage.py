c7ab856035f4362430ca9ef86e166e798fdc2a78
silljaco <161739321+silljaco@users.noreply.github.com>
2024-03-04 15:11:26 +0100

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,13 +6,13 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-
+'''
 while principal > 0:
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
 
 print('Total paid', total_paid)
-
+'''
 
 ### with 1000$ extra payment for the first 12 months
 
@@ -24,9 +24,9 @@ while principal > 0:
         total_paid = total_paid + payment + 1000
         months = months +1
     else:
-         principal = principal * (1+rate/12) - payment
-         total_paid = total_paid + payment
-         months = months +1
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+        months = months +1
 
 print ('Total paid:', total_paid, '$')
 print ('In', months, 'months')
