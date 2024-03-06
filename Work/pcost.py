70799f397aee0c7623ee2ee3e0657f6f85e4fd38
silljaco <161739321+silljaco@users.noreply.github.com>
2024-03-06 17:04:12 +0100

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,7 +2,14 @@
 #
 # Exercise 1.27
 
-
+'''
 with open('Data/portfolio.csv', 'rt') as f:
   for line in f:
     print(line, end='')
+'''
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f)
+headers
+for line in f:
+  print(line, end='')
