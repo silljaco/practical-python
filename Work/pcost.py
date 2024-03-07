e7092d08f10925515cacb6e24633e6ce784efadf
silljaco <161739321+silljaco@users.noreply.github.com>
2024-03-07 16:02:43 +0100

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -20,19 +20,25 @@ f.close()
 '''
 
 
-def portfolio_cost(filname):
-
+import csv
+def portfolio_cost(filename):
   total_cost = 0.0
   
-  with open(filename, 'rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        nshares = int(row[1])
-        price = float(row[2])
-        total_cost += nshares * price
-    return total_cost
-      
-cost = portfolio_cost('Data/portfolio.csv')
-print('Total cost', total_cost)
+  with open(filename, 'rt')as f:
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
+      nshares = int(row[1])
+      price = float(row[2])
+      total_cost += nshares * price
+  return total_cost
+
+filename = input('Enter filename')
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
+
+
+    
 
