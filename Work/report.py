8b23dea674fc4bdeb661dc1d0160702374dd7199
silljaco <161739321+silljaco@users.noreply.github.com>
2024-03-14 14:58:05 +0100

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,24 @@
 # report.py
 #
 # Exercise 2.4
+
+
+import csv
+
+def portfolio_cost(filename):
+  '''Calculates the total cost (shares*price) of a portfolio file'''
+  portfolio = []
+
+  with open(filename, 'rt') as f:
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
+      holding = (row[0], int(row[1]), float(row[2]))
+      portfolio.append(holding)
+      
+      return portfolio
+                 
+  
+
+
+
