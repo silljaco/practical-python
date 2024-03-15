1982d575d2a320f4d466b8849c95fcd3cb98634b
silljaco <161739321+silljaco@users.noreply.github.com>
2024-03-15 11:06:14 +0100

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -13,9 +13,22 @@ def read_portfolio(filename):
   with open(filename, 'rt') as f:
     rows = csv.reader(f)
     headers = next(rows)
+    
+    ''' 
+    ### CREATING TUPLES ###
     for row in rows:
       holding = (row[0], int(row[1]), float(row[2]))
       portfolio.append(holding)
+      '''
+    
+    ### CREATING DICTIONARIES ###
+  for row in rows:
+    stock = {
+      'name' : row[0],
+      'shares': int(row[1]),
+      'price' : float(row[2])
+    }
+    portfolio.append(stock)
       
   return portfolio
 
