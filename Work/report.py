34d03c51c12a28f6d855313280446056922cc16a
silljaco <161739321+silljaco@users.noreply.github.com>
2024-03-15 11:08:23 +0100

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -22,13 +22,13 @@ def read_portfolio(filename):
       '''
     
     ### CREATING DICTIONARIES ###
-  for row in rows:
-    stock = {
-      'name' : row[0],
-      'shares': int(row[1]),
-      'price' : float(row[2])
-    }
-    portfolio.append(stock)
+    for row in rows:
+      stock = {
+        'name' : row[0],
+        'shares': int(row[1]),
+        'price' : float(row[2])
+      }
+      portfolio.append(stock)
       
   return portfolio
 
